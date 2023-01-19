be60b725b110e7bd4e3681cda46d4ed6ce836042
sppham <phamsyphuc123@gmail.com>
2023-01-19 14:13:01 +0900

    Update eval_TTA.py

--- eval_TTA.py
+++ eval_TTA.py
@@ -2,3 +2,37 @@ import os
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
+
+import numpy as np
+import cv2
+import pandas as pd
+from glob import glob
+from tqdm import tqdm
+import tensorflow as tf
+from tensorflow.keras.utils import CustomObjectScope
+from sklearn.metrics import accuracy_score, f1_score, jaccard_score, precision_score, recall_score
+from metrics import dice_loss, dice_coef, iou
+from train import create_dir, load_data
+
+# image size
+H = 256
+W = 256
+
+def read_image(path):
+    x = cv2.imread(path, cv2.IMREAD_COLOR)  ## (H, W, 3)
+    x = cv2.resize(x, (W, H))
+    ori_x = x
+    x = x/255.0
+    x = x.astype(np.float32)
+    x = np.expand_dims(x, axis=0)
+    return ori_x, x                                ## (1, 256, 256, 3)
+  
+  
+def read_mask(path):
+    x = cv2.imread(path, cv2.IMREAD_GRAYSCALE)  ## (H, W)
+    x = cv2.resize(x, (W, H))
+    ori_x = x
+    x = x/255.0
+    x = x.astype(np.int32)                    ## (256, 256)
+    return ori_x, x
